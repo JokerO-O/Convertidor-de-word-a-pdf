fb8dc187e69435c4bdd5d0b2ac63ea0717b9d711
JokerO-O <universidaditla4@gmail.com>
2024-09-27 11:40:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -115,3 +115,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Crear todas las tablas una vez
     app.run(debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True)
